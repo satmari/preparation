4d41cfa7c49015e85ed63d113ee968043abb69c2
satmari <satmariatila@gmail.com>
2016-04-18 09:36:31 +0200

    2016.04.18

--- resources/views/main/edit.blade.php
+++ resources/views/main/edit.blade.php
@@ -44,11 +44,12 @@
 					<span>Total Order Qty:</span>
 					{!! Form::input('number', 'total_order_qty', null, ['class' => 'form-control']) !!}
 				</div>
+				@endif
 				<div class="panel-body">
 					<span>Flash:</span>
 					{!! Form::input('boolean', 'flash', null, ['class' => 'form-control']) !!}
 				</div>
-				@endif
+				
 				<div class="panel-body">
 					<span>Closed Po:</span>
 					{{-- {!! Form::input('boolean', 'closed_po', null, ['class' => 'form-control']) !!} --}}
